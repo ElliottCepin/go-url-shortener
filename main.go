d3730ccd14e0e367022bf06ac53ff51d4b81c2a8
ElliottCepin <elliottcepin5@gmail.com>
2026-03-11 12:45:06 -0700

    Response codes for POST /shorten working

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 )
 
 func shorten(w http.ResponseWriter, r *http.Request) {
-		
+	if (r.Method != "POST") {
+		w.WriteHeader(http.StatusMethodNotAllowed) // I'd rather just write 405, but whatever	
+	}
 }
 
 func main() {
